cc3a17f96fd8df9d9e04748e0e7eaa397b7c5e7f
seth808rose <69048387+seth808rose@users.noreply.github.com>
2022-01-21 14:30:29 -0800

    fixed app page issues

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
 import React from "react";
 import TopBar from "./topbar/TopBar";
-import Header from "./header/Header";
 import Home from "./pages/home/Home";
 import Single from "./single/Single";
-import Post from "../src/post/Post";
 import Create from "../src/create/Create";
-import Setup from "../src/setup/Setup";
-import Write from "../src/";
 import Login from "../src/login/Login";
 import Register from "./register/Register";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { Link } from "react-router-dom";
 import Contact from "./contact/Contact";
 
 const App = () => {
